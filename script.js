7ec6dc444cbb04f083f1b95a7f09512895c9b65f
aishik <1804146@kiit.ac.in>
2023-03-20 01:29:41 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == 'C'){
+      string = ""
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == 'X'){
+      console.log(e.target)
+      string =  string + '*';
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == '÷'){
+      console.log(e.target)
+      string =  string + '/';
+      document.querySelector('input').value = string;
+    }
+    else{ 
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+      }
+  })
+})
